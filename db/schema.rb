ef11d19ecce8923d12becaa642db37314b051296
Claudio Faoro <mail@claudiofaoro.com>
2021-02-01 18:06:17 +0100

    Added Contents scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_155421) do
+ActiveRecord::Schema.define(version: 2021_02_01_091319) do
+
+  create_table "contents", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
